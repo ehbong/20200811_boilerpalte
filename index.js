f3c7e661134d2abcbdb20ad6641d533c32aa778b
ehbong <hangillw86@gmail.com>
2020-08-12 00:22:34 +0900

    Create Project And Connect MongoDB

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 5050
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://ehbong:q1w2e3r4@boilerpalte.xiue1.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=> console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+app.get('/', (req, res)=> res.send('Hello World!~안녕하세요 ~ '));
+app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
+
+
+
